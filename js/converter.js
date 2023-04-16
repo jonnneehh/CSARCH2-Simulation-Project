68f5065fddec785c80d4c958fed6b6b08ec3e28a
jonnneehh <jon_llamado@dlsu.edu.ph>
2023-04-16 12:31:15 +0800

    should work now. no testing done

--- js/converter.js
+++ js/converter.js
@@ -1,155 +1,214 @@
-var input_mantissa = document.getElementById("mantissa-input").value;
-var input_exponent = document.getElementById("exponent").value
+$(document).ready(function () {
+    //Copies the hex text
+    $("#copy-hex").click(function (){
+        var outputBinary = $("#output-hex-text")
 
+        var tempTextarea = $("<textarea>");
+        tempTextarea.val(outputBinary.text());
 
-function roundToNearest(mantissa){
-    let bits = mantissa.split('');
-    let lastBit = bits[bits.length-1];
+        outputBinary.select()
+        document.execCommand("copy");
+        $("body").append(tempTextarea);
+        tempTextarea.select();
+        document.execCommand("copy");
+        tempTextarea.remove();
 
-    bits = bits.slice(0, bits.length - 1);
-    let roundUp = false;
-    for (let i = bits.length - 1; i >= 0; i--) {
-        if (bits[i] === '1') {
-            if (!roundUp) {
-                roundUp = true;
-            }
-        }
-        else if (roundUp) {
-            bits[i] = '1';
-            roundUp = false;
-        }
-        if (i === bits.length - 53) {
-            break;
-        }
-    }
-    if (roundUp) {
-        lastBit = '1';
-    }
-    bits.push(lastBit);
-    return bits.join('');
-}
+        //Simulate something here to show button is copied
+    })
 
-function normalize(mantissa, exponent) {
-    //get sign
-    let sign = mantissa[0] === '-' ? -1 : 1; //if neg, -1, else, 1
+    //Copies the binary text
+    $("#copy-binary").click(function (){
+        var outputBinary = $("#output-binary-text")
 
-    //remove -
-    mantissa = mantissa.replace('-', ''); 
+        var tempTextarea = $("<textarea>");
+        tempTextarea.val(outputBinary.text());
 
-    //get position of decimal point
-    let decimalPos = mantissa.indexOf('.');
-    if (decimalPos === -1) {
-        decimalPos = mantissa.length;
-    }
+        outputBinary.select()
+        document.execCommand("copy");
+        $("body").append(tempTextarea);
+        tempTextarea.select();
+        document.execCommand("copy");
+        tempTextarea.remove();
 
-    //get number of bits to shift point
-    let shift = decimalPos - exponent;
-
-    //if shift is neg, add zeroes to beginning of mantissa
-    if (shift < 0) {
-        mantissa = '0'.repeat(-shift) + mantissa;
-        shift = 0;
-    }
-
-    //Shift point left or right
-    mantissa = mantissa.replace('.', ''); //remove .
-    if (shift > 0) {
-        mantissa = mantissa.padEnd(shift + mantissa.length, '0');
-    } else {
-        mantissa = mantissa.padStart(shift + mantissa.length, '0');
-    }
-    mantissa = mantissa.slice(0, mantissa.length - 52) + '.' + mantissa.slice(mantissa.length - 52);
-
-    //round mantissa
-    mantissa = roundToNearest(mantissa);
-
-    //if mantissa rounded up, shift decimal point, add exponent
-    if (mantissa === '1') {
-        mantissa = '0.' + '0'.repeat(52);
-        exponent++;
-    }
-
-    //adjust sign of exponent
-    exponent *= sign;
-
-    return {mantissa: mantissa, exponent: exponent};
-}
-
-function converttoBinary64(mantissa, exponent) { 
-    let signBit = mantissa[0] === '-' ? '1': '0';
+        //Simulate something here to show text is copied
+    })
     
-    //normalize mantissa
-    let normalized = normalize(mantissa, exponent);
-    let normalizedMantissa = normalized.mantissa;
-    let normalizedExponent = normalized.exponent;
+    //Does the function
+    $("#submit-input").click(function () {
+        var mantissa = $("#mantissa-input").val() 
+        var exponent = $("#exponent-input").val()
 
-    //get e'
-    let biasedExponent = normalizedExponent + 1023;
+        var outputBinary = $("#output-binary-text")
+        var outputHexadecimal = $("#output-hex-text")
+        
+        var error_div = $(".input-error")
+        var error_text = $("#input-error-text")
 
-    //Convert e' to binary and pad to 11 bits
-    let eprime = biasedExponent.toString(2).padStart(11, '0');
+        if(mantissa != "" && exponent != "") {
+            error_div.css("visibility", "hidden")
+            
+            actual_output = converttoBinary64(mantissa, exponent)
+            outputBinary.text(actual_output)
+            outputHexadecimal.text(converttoHex(actual_output))
+        }   
+        else{
+            error_text.text("Error: Please fill out the textbox")
+            error_div.css("visibility", "visible")
+        }   
+        
+        if(actual_output) $(".area-output").css("visibility", "visible")
+
+        function roundToNearest(mantissa){
+            let bits = mantissa.split('');
+            let lastBit = bits[bits.length-1];
     
-    //pad normalized mantissa to 52 bits
-    let binaryMantissaPadded = normalizedMantissa.replace('.', '').padEnd(52, '0');
-
-    //perform checks
-    let status = '';
-    if (eprime === '11111111111') {
-        if (binaryMantissaPadded === '0000000000000000000000000000000000000000000000000000') {
-            if (signBit === '0') {
-                status = '+inf';
-                return status;
+            bits = bits.slice(0, bits.length - 1);
+            let roundUp = false;
+            for (let i = bits.length - 1; i >= 0; i--) {
+                if (bits[i] === '1') {
+                    if (!roundUp) {
+                        roundUp = true;
+                    }
+                }
+                else if (roundUp) {
+                    bits[i] = '1';
+                    roundUp = false;
+                }
+                if (i === bits.length - 53) {
+                    break;
+                }
             }
-            else {
-                status = '-inf';
-                return status;
+            if (roundUp) {
+                lastBit = '1';
             }
+            bits.push(lastBit);
+            return bits.join('');
         }
-    }
     
-    if (eprime === '00000000000') { //e' == 0
-        if (binaryMantissaPadded === '0000000000000000000000000000000000000000000000000000') {
-            if (signBit === '0') {
-                status = '+0';
-                return status;
+        function normalize(mantissa, exponent) {
+            //get sign
+            let sign = mantissa[0] === '-' ? -1 : 1; //if neg, -1, else, 1
+    
+            //remove -
+            mantissa = mantissa.replace('-', ''); 
+    
+            //get position of decimal point
+            let decimalPos = mantissa.indexOf('.');
+            if (decimalPos === -1) {
+                decimalPos = mantissa.length;
             }
-            else {
-                status = '-0';
-                return status;
+    
+            //get number of bits to shift point
+            let shift = decimalPos - exponent;
+    
+            //if shift is neg, add zeroes to beginning of mantissa
+            if (shift < 0) {
+                mantissa = '0'.repeat(-shift) + mantissa;
+                shift = 0;
             }
-        }
-        else { //e' == zero, but significand != 0
-            status = 'Denormalized';
-            return status;
-        }
-    }
     
-    //combine 
-    let binary = signBit + eprime + binaryMantissaPadded;
-
-    return binary;
-}
-
-
-function converttoHex(bits){
-    let hex = '';
-    for (let i = 0; i < 16; i++) {
-        let slice = bits.slice(i * 4, (i + 1) * 4);
-        let nibble = parseInt(slice, 2).toString(16);
-        hex += nibble;
-    }
-
-    return hex;
-}
-
-
-function returnOutput() {
-    subprocess = '';
-    output = '';
+            //Shift point left or right
+            mantissa = mantissa.replace('.', ''); //remove .
+            if (shift > 0) {
+                mantissa = mantissa.padEnd(shift + mantissa.length, '0');
+            } else {
+                mantissa = mantissa.padStart(shift + mantissa.length, '0');
+            }
+            mantissa = mantissa.slice(0, mantissa.length - 52) + '.' + mantissa.slice(mantissa.length - 52);
     
-    subprocess = normalize(input_mantissa, input_exponent);
-    output = binarytofloat64(subprocess[0], subprocess[1]);
-    document.getElementById('output-binary-text').innerHTML = output;
-}
+            //round mantissa
+            mantissa = roundToNearest(mantissa);
+    
+            //if mantissa rounded up, shift decimal point, add exponent
+            if (mantissa === '1') {
+                mantissa = '0.' + '0'.repeat(52);
+                exponent++;
+            }
+    
+            //adjust sign of exponent
+            exponent *= sign;
+    
+            return {mantissa: mantissa, exponent: exponent};
+        }
+    
+        function converttoBinary64(mantissa, exponent) { 
+            let signBit = mantissa[0] === '-' ? '1': '0';
+            
+            //normalize mantissa
+            let normalized = normalize(mantissa, exponent);
+            let normalizedMantissa = normalized.mantissa;
+            let normalizedExponent = normalized.exponent;
+    
+            //get e'
+            let biasedExponent = normalizedExponent + 1023;
+    
+            //Convert e' to binary and pad to 11 bits
+            let eprime = biasedExponent.toString(2).padStart(11, '0');
+            
+            //pad normalized mantissa to 52 bits
+            let binaryMantissaPadded = normalizedMantissa.replace('.', '').padEnd(52, '0');
+    
+            //perform checks
+            let status = '';
+            if (eprime === '11111111111') {
+                if (binaryMantissaPadded === '0000000000000000000000000000000000000000000000000000') {
+                    if (signBit === '0') {
+                        status = '+inf';
+                        return status;
+                    }
+                    else {
+                        status = '-inf';
+                        return status;
+                    }
+                }
+            }
+            
+            if (eprime === '00000000000') { //e' == 0
+                if (binaryMantissaPadded === '0000000000000000000000000000000000000000000000000000') {
+                    if (signBit === '0') {
+                        status = '+0';
+                        return status;
+                    }
+                    else {
+                        status = '-0';
+                        return status;
+                    }
+                }
+                else { //e' == zero, but significand != 0
+                    status = 'Denormalized';
+                    return status;
+                }
+            }
+            
+            //combine 
+            let binary = signBit + eprime + binaryMantissaPadded;
+    
+            return binary;
+        }
+    
+    
+        function converttoHex(bits){
+            let hex = '';
+            for (let i = 0; i < 16; i++) {
+                let slice = bits.slice(i * 4, (i + 1) * 4);
+                let nibble = parseInt(slice, 2).toString(16);
+                hex += nibble;
+            }
+    
+            return hex;
+        }
+    
+    
+        function returnOutput() {
+            subprocess = '';
+            output = '';
+            
+            subprocess = normalize(input_mantissa, input_exponent);
+            output = binarytofloat64(subprocess[0], subprocess[1]);
+            document.getElementById('output-binary-text').innerHTML = output;
+        }
+    });
+})
+
 
 
